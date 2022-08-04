b7ca94518f294d09b1c03f740dc45dc67e489051
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-04 12:28:53 +0530

    edited by Adriel
    source:freecodecamp.org

---
+++ 3_modules.js
@@ -0,0 +1,14 @@
+//Modules=function in js
+
+const adriel = 'Adriel'
+const arslaan = 'Arslaan'
+const carlton = 'Carlton'
+
+const sayHi =(name) => {
+   console.log(`Hello there ${name}`)
+}
+
+sayHi('Pooja')
+sayHi(adriel)
+sayHi(arslaan)
+sayHi(carlton)
