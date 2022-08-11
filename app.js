7ddb8823b07130312ec093fa276c8ee2e82e93d3
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-11 14:59:38 +0530

    edited by adriel
    mades some changes in code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const names = require('./4_names')
-const sayHi = require('./5_utils')
-const data = require('./6_alt_flavor')
+const names = require('./5_names')  //require is used to importing
+const sayHi = require('./7_utils')
+const data = require('./6_alt-flavor')
 console.log(data);
 
 sayHi('Pooja')
-sayHi(names.arslaan)
-sayHi(names.carlton)
+sayHi(names.arslaan)  //called through object,name is a object
+sayHi(names.carlton)    
