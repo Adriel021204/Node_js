8d168ab539725ebba99453d24ff1d852761c15d6
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-11 15:03:24 +0530

    edited by adriel
    changes made in code

---
+++ 4_modules.js
@@ -0,0 +1,14 @@
+//Modules=function in js
+
+const adriel = 'Adriel'
+const arslaan = 'Arslaan'
+const carlton = 'Carlton'
+
+const sayHi =(name) => {               //sayHi funct in js
+   console.log(`Hello there everyone!${name}`)
+}
+
+sayHi('Pooja')
+sayHi(adriel)
+sayHi(arslaan)
+sayHi(carlton)
