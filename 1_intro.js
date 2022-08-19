66852eb0ba96bb53e395a91599ce201a1a715929
Adriel Fernandes <adrielferns021204@gmail.com>
2022-08-19 13:26:26 +0530

    edited by adriel

--- 1_intro.js
+++ 1_intro.js
@@ -1,5 +1,5 @@
 let MessageToEveryone = "Hello World!!!"
 const FirstTime = "My first NodeJs code."
 
-console.log(MessageToEveryone);    //window.console.log in js
+console.log(MessageToEveryone);    //window.console.log is also used in js
 console.log(FirstTime);            //console.log is a global object 
