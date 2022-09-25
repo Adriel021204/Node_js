db595ffcb69bdaeae534c4585ca8ebc841fefc9a
Adriel Fernandes <110371966+Adriel021204@users.noreply.github.com>
2022-09-25 23:00:08 +0530

    Delete 1_intro.js

--- 1_intro.js
+++
@@ -1,6 +0,0 @@
-var message = 'bello blah bruh';
-let messageToEveryone = 'Hello World!!!';
-const firstTime = 'My first NodeJs code';
-
-console.log(MessageToEveryone);    //window.console.log is also used in js
-console.log(FirstTime);            //console.log is a global object 
