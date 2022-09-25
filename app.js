c91a73e97c7d6351d99692bd0f5a961974877319
Adriel Fernandes <110371966+Adriel021204@users.noreply.github.com>
2022-09-25 23:01:42 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const names = require('./5_names')  //require is used to importing
-const sayHi = require('./7_utils')
-const data = require('./6_alt-flavor')
-console.log(data);
-
-sayHi('Pooja')
-sayHi(names.arslaan)  //called through object,name is a object
-sayHi(names.carlton)    
